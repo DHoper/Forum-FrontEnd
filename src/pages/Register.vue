81620a1060d0a9efd108ad0dbae29ea5fccb012b
DHoper <gxgemini777@gmail.com>
2023-11-03 16:11:16 +0800

    opt-優化假資料部分

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -6,58 +6,11 @@ import { useLoadingStore } from "../store/loading";
 import { useUserStore } from "../store/user";
 import { postUserData } from "../api/user/user.ts";
 import { FieldName, inputValidator } from "../utils/validator";
+import { topicTags } from "../fakeData/topicTags.js";
 
 const loadingStore = useLoadingStore();
 const router = useRouter();
 
-const topicTags = {
-  ecology: {
-    color: "#A89376",
-    tags: [
-      "生態系",
-      "水生生物",
-      "魚類",
-      "陸生動物",
-      "節肢動物",
-      "兩棲動物",
-      "植物",
-      "瀕危物種",
-      "危險動物",
-      "鳥類",
-    ],
-  },
-  knowledge: {
-    color: "#875B4A",
-    tags: ["動物行為", "遷徒", "奇特行為", "攝影技巧"],
-  },
-  geographicalFeatures: {
-    color: "#B7AFA6",
-    tags: [
-      "野生動物保護區",
-      "海洋",
-      "溪河湖泊",
-      "山林",
-      "森林",
-      "草原",
-      "雪地",
-      "沙漠",
-      "海島",
-      "沼澤",
-    ],
-  },
-  style: {
-    color: "#566E3D",
-    tags: ["自然風景", "夜間攝影", "微距攝影", "野外探險", "地理風貌"],
-  },
-  issue: {
-    color: "#22577A",
-    tags: ["環境保育", "全球生態", "嚴重議題"],
-  },
-  geolocation: {
-    color: "#F4B860",
-    tags: ["非洲", "亞洲", "南美洲", "北美洲", "歐洲", "澳大利亞", "南極洲"],
-  },
-};
 
 //表單註冊
 const formInput = ref({
--- src/pages/User/PersonalInfo.vue
+++ src/pages/User/PersonalInfo.vue
@@ -7,6 +7,7 @@ import { FieldName, inputValidator } from "../../utils/validator";
 import { getUserData } from "../../api/user/user.js";
 import Dialog from "../../components/utils/Dialog.vue";
 import { UserDataType } from "../../types";
+import { topicTags } from "../../fakeData/topicTags.js";
 
 const route = useRoute();
 const props = defineProps({
@@ -31,54 +32,6 @@ const fetchUserData = async () => {
   }
 };
 
-const topicTags = {
-  ecology: {
-    color: "#A89376",
-    tags: [
-      "生態系",
-      "水生生物",
-      "魚類",
-      "陸生動物",
-      "節肢動物",
-      "兩棲動物",
-      "植物",
-      "瀕危物種",
-      "危險動物",
-      "鳥類",
-    ],
-  },
-  knowledge: {
-    color: "#875B4A",
-    tags: ["動物行為", "遷徒", "奇特行為", "攝影技巧"],
-  },
-  geographicalFeatures: {
-    color: "#B7AFA6",
-    tags: [
-      "野生動物保護區",
-      "海洋",
-      "溪河湖泊",
-      "山林",
-      "森林",
-      "草原",
-      "雪地",
-      "沙漠",
-      "海島",
-      "沼澤",
-    ],
-  },
-  style: {
-    color: "#566E3D",
-    tags: ["自然風景", "夜間攝影", "微距攝影", "野外探險", "地理風貌"],
-  },
-  issue: {
-    color: "#22577A",
-    tags: ["環境保育", "全球生態", "嚴重議題"],
-  },
-  geolocation: {
-    color: "#F4B860",
-    tags: ["非洲", "亞洲", "南美洲", "北美洲", "歐洲", "澳大利亞", "南極洲"],
-  },
-};
 const formData = ref();
 const setInputData = () => {
   if (userData.value) {
